181a01980aa7fc9c991000cecde9435b728c5b17
Anthony Liang <antliang1117@gmail.com>
2022-01-06 17:19:21 -0500

    Add README, switch Image source to uri and add image, instructions styles to styles obj

--- App.js
+++ App.js
@@ -7,14 +7,18 @@ export default function App() {
     <View style={styles.container}>
       {/*
         Adding a image tag to display image from assets
+        or can use {uri: <link to image from web>}
       */}
-      <Image source={logo} />
+      <Image
+        source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+        style={styles.image}
+      />
 
       {/*
         Going to change OG text to instruct our users how to use the app
         Adding style by adding an obj that contains styling
       */}
-      <Text style={{ color: "#888", fontSize: 18 }}>
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
@@ -31,4 +35,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  // marginBottom to have some space between image and text
+  image: { width: 305, height: 159, marginBottom: 15 },
+
+  // marginHorizontal to have some space around the edges of the screen
+  instructions: { color: "#888", fontSize: 28, marginHorizontal: 15 },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# Image Share
+
+Created this project while learning React Native <img src="https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg" width="18px">
